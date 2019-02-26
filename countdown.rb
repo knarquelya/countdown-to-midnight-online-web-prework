72aa355afcafd662bc2561f694fc34b2a145c54b
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-26 17:17:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+number = 0
 
-def countdown
+def countdown 
+  while number > 0 
+    puts "#{number} SECOND(S)"
+    number -= 1 
+  end
+end
+
+  puts "HAPPY NEW YEAR!"
 end
